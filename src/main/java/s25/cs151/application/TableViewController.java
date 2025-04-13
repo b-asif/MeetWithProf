e2a9d8114888fc2fa655dff94045b73c7f5c4e33
b-asif <62410685+b-asif@users.noreply.github.com>
2025-04-12 18:02:12 -0700

    changed the file directory names

--- src/main/java/s25/cs151/application/TableViewController.java
+++ src/main/java/s25/cs151/application/TableViewController.java
@@ -166,7 +166,7 @@ private void loadTimeSlot() {
 
     private void loadDataEntries() {
         dataEntries.clear();
-        String query = "SELECT * FROM data_entry";
+        String query = "SELECT * FROM data_entry ORDER BY date ASC, time ASC";
         try(Connection conn = DriverManager.getConnection("jdbc:sqlite:data_entry.db");
             Statement stmt = conn.createStatement();
             ResultSet result = stmt.executeQuery(query)) {
